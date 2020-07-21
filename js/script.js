4310625d9ab83a0421e1327ae5ede437833df826
Michal Sutula <michal.sutula@gmail.com>
2020-07-21 11:36:47 +0200

    Add class

--- js/script.js
+++ js/script.js
@@ -2,18 +2,22 @@
 
 function titleClickHandler(event) {
     console.log('Link was clicked!');
+    const clickedElement = this;
     console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
 
-    /* remove class 'active' from all articles */
+    clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
 
     for (let activeArticle of activeArticles) {
