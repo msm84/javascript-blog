d529dec21303eb4789ab7d361f49eb5b7b265664
Michal Sutula <michal.sutula@gmail.com>
2020-07-28 19:05:40 +0200

    Add split, but there is some problem.

--- js/script.js
+++ js/script.js
@@ -44,7 +44,8 @@ function titleClickHandler(event) {
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list'; //lista Ul zawierająca tagi poszczególnych artykułów//
 
 function generateTitleLinks() {
 
@@ -79,3 +80,42 @@ function generateTitleLinks() {
 }
 
 generateTitleLinks();
+
+// Module 6 //
+
+function generateTags(){
+  /* [DONE] find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* [DONE] START LOOP: for every article: */
+  for (let article of articles) {
+    /* [DONE] find tags wrapper */
+    const titleList = article.querySelector(optArticleTagsSelector);
+    /* [DONE] make html variable with empty string */
+    let html ='';
+    /* [DONE] get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
+    /* [DONE] split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
+
+    /* START LOOP: for each tag */
+    for(let tag of articleTagsArray){
+    /* generate HTML of the link */
+    console.log(tag);
+    }
+
+
+
+    /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+  }
+}
+
+generateTags();
